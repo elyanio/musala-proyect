2a0ed480f355ba0d6a966928259e4f87a18269a6
Yanier Alfonso <48162982+elyanio@users.noreply.github.com>
2020-06-25 11:53:27 -0400

    add crypt system to encrypt password

--- src/graphql/resolvers/Mutations/signUp.ts
+++ src/graphql/resolvers/Mutations/signUp.ts
@@ -6,12 +6,14 @@ import CustomError from '../../../model/error/CustomError';
 
 const signUp = async (
   _: undefined,
-  { data: { email }, data }: MutationResolvers.ArgsSignUp,
+  { data: { email, password }, data }: MutationResolvers.ArgsSignUp,
   { prisma, services }: Context,
 ): Promise<User> => {
   const exist = await prisma.$exists.user({ email });
   if (exist) throw new CustomError('DUPLICATED_USER');
-  const user = await prisma.createUser({ ...data, role: RolesEnum.USER });
+  const planPassword = services.auth.decrypt(password);
+  const hash = await services.auth.hash(planPassword);
+  const user = await prisma.createUser({ ...data, password: hash, role: RolesEnum.USER });
   const token = services.auth.tokenFromId(user?.id);
   const authenticatedUser = prisma.updateUser({
     data: { token },
